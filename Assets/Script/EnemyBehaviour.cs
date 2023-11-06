c9c1aad6bbbd347ed6a67ee97b1f11bfe01b94b1
raktyk39 <142972197+raktyk39@users.noreply.github.com>
2023-11-06 13:09:16 +0200

    HEro-Born
    ssdfsdf

--- Assets/Script/EnemyBehaviour.cs
+++ Assets/Script/EnemyBehaviour.cs
@@ -1,30 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    void OnTriggerEnter (Collider other) { // с тригером
-        
-       if (  other.name == "Player")
-       {
+    public Transform patrolRoute;
+    public List<Transform> locations;
+    private int localIndex = 0;
+    private NavMeshAgent agent;
 
-       Debug.Log( "Player detected - attack!");
-
-       }
-        
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
     }
 
-  
-     void OnTriggerExit(Collider other) {
-        
-      if ( other.name == "Player") {
-
-         Debug.Log("Player out of range, resume patro");
-
-      }
-
-
+    void Update ()
+    {
+      
     }
 
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        agent.destination = locations[localIndex].position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player detected - attack!");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player out of range, resume patrol");
+        }
+    }
 }
